ef219314f836ee1e6fa2522bdc1eba837ff10635
Adam Samec <adam.samec@gmail.com>
2024-03-30 10:00:47 +0100

    Add download in progress dialog without logic yet

---
+++ WinManager/UpdateDownloadInProgressDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WinManager
+{
+    /// <summary>
+    /// Interaction logic for UpdateDownloadInProgressDialog.xaml
+    /// </summary>
+    public partial class UpdateDownloadInProgressDialog : Window
+    {
+        public UpdateDownloadInProgressDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateDownloadInProgressDialog_Loaded(object sender, RoutedEventArgs e)
+        {   
+            continueUpdateDownloadButton.Focus();
+        }
+
+        private void cancelUpdateDownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+        private void continueUpdateDownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
